da0dbf5f18d66d70c8e1490c2d54179d1f5a3f5e
panzj <panzj@yxt.com>
2022-07-14 09:57:35 +0800

    keep-alive understanding done

--- src/core/components/keep-alive.js
+++ src/core/components/keep-alive.js
@@ -28,11 +28,14 @@ function matches (pattern: string | RegExp | Array<string>, name: string): boole
 }
 
 function pruneCache (keepAliveInstance: any, filter: Function) {
+  // keepAliveInstance 上保存有缓存对象cache keys, 两者同步更新
   const { cache, keys, _vnode } = keepAliveInstance
   for (const key in cache) {
     const entry: ?CacheEntry = cache[key]
     if (entry) {
       const name: ?string = entry.name
+      // 把不能通过filter检查的缓存项删除，
+      // 缓存项是componentInstance, 先$destroy(), 再从cache对象删除
       if (name && !filter(name)) {
         pruneCacheEntry(cache, key, keys, _vnode)
       }
@@ -70,13 +73,16 @@ export default {
     cacheVNode() {
       const { cache, keys, vnodeToCache, keyToCache } = this
       if (vnodeToCache) {
+        // vnode上保存有 组件实例 组件选项
         const { tag, componentInstance, componentOptions } = vnodeToCache
+        // 缓存组件
         cache[keyToCache] = {
           name: getComponentName(componentOptions),
           tag,
           componentInstance,
         }
         keys.push(keyToCache)
+        // 超出最大缓存数，删掉首部最旧的组件
         // prune oldest entry
         if (this.max && keys.length > parseInt(this.max)) {
           pruneCacheEntry(cache, keys[0], keys, this._vnode)
@@ -92,12 +98,14 @@ export default {
   },
 
   destroyed () {
+    // keep-alive组件销毁后，清除缓存对象cache，缓存的组件也执行$destroy()
     for (const key in this.cache) {
       pruneCacheEntry(this.cache, key, this.keys)
     }
   },
 
   mounted () {
+    // 缓存子组件 并 监听 include 和 exclude 的变化
     this.cacheVNode()
     this.$watch('include', val => {
       pruneCache(this, name => matches(val, name))
@@ -113,10 +121,12 @@ export default {
 
   render () {
     const slot = this.$slots.default
-    const vnode: VNode = getFirstComponentChild(slot)
+    // 获取第一个子组件 <keep-alive></keep-alive>内部只支持单个组件
+    const vnode: VNode = getFirstComponentChild(slot) 
     const componentOptions: ?VNodeComponentOptions = vnode && vnode.componentOptions
     if (componentOptions) {
       // check pattern
+      // 用组件名作为判断是否应该缓存的依据
       const name: ?string = getComponentName(componentOptions)
       const { include, exclude } = this
       if (
@@ -125,21 +135,29 @@ export default {
         // excluded
         (exclude && name && matches(exclude, name))
       ) {
+        // kk: 子组件不在缓存范围内 则直接返回子组件的虚拟节点，即渲染子组件
         return vnode
       }
 
       const { cache, keys } = this
+      // 缓存对象 cache[vnode.key] = {name, tag, componentInstance: vnode.componentInstance }
       const key: ?string = vnode.key == null
         // same constructor may get registered as different local components
         // so cid alone is not enough (#3269)
         ? componentOptions.Ctor.cid + (componentOptions.tag ? `::${componentOptions.tag}` : '')
         : vnode.key
-      if (cache[key]) {
+
+        if (cache[key]) {
+        // kk: 子组件在缓存范围内  且 有缓存；则修改vnode.componentInstance为缓存的组件实例 
+        // 则渲染vnode，得到的是旧组件
         vnode.componentInstance = cache[key].componentInstance
+        // 最新使用的组件，key放在末尾，则超出最大缓存数时，删掉头部最旧的缓存组件
         // make current key freshest
         remove(keys, key)
         keys.push(key)
       } else {
+        // kk: 子组件在缓存范围内 但 无缓存； 则 设置vnode为待缓存
+        // 生命周期  render -> mounted  / render -> updated
         // delay setting the cache until update
         this.vnodeToCache = vnode
         this.keyToCache = key
